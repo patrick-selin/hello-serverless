0ccc29b7fade1234a4e7e3cf7b52a22470f1890e
Patrick Selin <patrick.selin@gmail.com>
2025-02-23 08:34:58 +0200

    init commit

---
+++ test/hello-serverless.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as HelloServerless from '../lib/hello-serverless-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/hello-serverless-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new HelloServerless.HelloServerlessStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
